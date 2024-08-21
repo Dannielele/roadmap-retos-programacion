f3bde5be621fb06e509a34a2dec7f1e1614c7305
Dannielele <danielvive2019@gmail.com>
2024-08-20 18:16:49 -0600

    Dannielele.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Dannielele.py
@@ -0,0 +1,38 @@
+# enlace de pagina web : https://www.python.org/
+
+# Comentario en varias linea
+
+"""
+Comentario
+de 
+varias
+lineas
+"""
+
+'''
+Comentario
+en 
+varias 
+lineas
+'''
+# Variables y constante
+saludo = "Hola"
+despedida = "Adios"
+MY_CONSTANT = "Hola amigos"   #hacer una constante de variable
+
+# Datos primitivos
+
+# Int Número entero
+int_number = 12
+
+# Float Número de punto flotante o decimal
+float_number = 1.2
+
+# str o Cadena de texto
+string = ("Cadena de texto")
+
+#booleanos
+boolean_Ture = "si"
+boolean_False = "no"
+
+print("¡Hola, Python!")
